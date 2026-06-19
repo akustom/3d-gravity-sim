c8b53ec500419b5a7c251a634f03ca62e550beb9
akustom <dizzaastha@gmail.com>
2026-06-20 00:11:01 +0700

    nothing much

--- include/utilities.hpp
+++ include/utilities.hpp
@@ -4,9 +4,9 @@
 
 namespace util {
     struct vertex {
-        glm::vec3 pos;
+        glm::vec4 pos;
         glm::vec3 color;
 
-        vertex(glm::vec3 pos, glm::vec3 color) : pos(pos), color(color) {}
+        vertex(glm::vec3 pos, glm::vec3 color) : pos(pos, 1.0f), color(color) {}
     };
 }
