cfca0a9b5c873351cdc728c69dd71b7d420290dd
akustom <dizzaastha@gmail.com>
2026-06-20 00:10:31 +0700

    slowly incorporating UBO into the design WIP

--- assets/shaders/vertexShader.glsl
+++ assets/shaders/vertexShader.glsl
@@ -3,8 +3,10 @@ layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
 
 layout (location = 2) in vec3 displacement;
-uniform mat4 view;
-uniform mat4 projection;
+layout (std140) uniform Matrices {
+    mat4 view;
+    mat4 projection;
+};
 
 out vec3 vertexColor;
 
