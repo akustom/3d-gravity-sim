69bf84186668d2385819d30b6e8c07db52fbff3a
akustom <dizzaastha@gmail.com>
2026-06-21 17:20:46 +0700

    created particles

---
+++ include/physics/particle.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <vector>
+#include <glm/glm.hpp>
+
+namespace phy {
+    struct Particles {
+        std::vector<glm::dvec4> positions;
+        std::vector<glm::dvec4> velocities;
+        std::vector<glm::dvec4> accelerations;
+
+        std::vector<double> masses;
+
+        void createParticle(const double& mass, const glm::vec3& position) {
+            positions.emplace_back(position, 1);
+            velocities.emplace_back(0, 0, 0, 0);
+            accelerations.emplace_back(0, 0, 0, 0);
+
+            masses.push_back(mass);
+        }
+    };
+}
