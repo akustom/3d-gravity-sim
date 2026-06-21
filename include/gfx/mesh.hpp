f42c1b9264b21799ea4c037e0098a437abf1897c
akustom <dizzaastha@gmail.com>
2026-06-22 02:16:41 +0700

    created 3d sphere vertices generation (indices are wip)

--- include/gfx/mesh.hpp
+++ include/gfx/mesh.hpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <cmath>
 #include <glad/glad.h>
+// ReSharper disable once CppUnusedIncludeDirective
 #include <glm/glm.hpp>
 #include "vertex.hpp"
 
@@ -13,11 +14,11 @@ namespace gfx {
 
         std::vector<glm::uvec3> indices = {};
 
-        void loadModel() {}
+        void loadModel();
     };
 
-    /**turns your Mesh into a polygon of n-sides*/
-    inline void makePolygon(Mesh& mesh, const float size, const int sides) {
+    /**turns your Mesh into a regular polygon of n-sides*/
+    inline void makePolygon(Mesh& mesh, const float radius, const int sides) {
         if (sides < 3) return;
 
         std::vector<vertex> vertices;
@@ -31,8 +32,8 @@ namespace gfx {
         for (int i = 0; i < sides; ++i) {
             float currentAngle = angleIncrement * static_cast<float>(i);
             glm::vec3 pos = glm::vec3(
-                size * std::cos(currentAngle),
-                size * std::sin(currentAngle),
+                radius * std::cos(currentAngle),
+                radius * std::sin(currentAngle),
                 0.0f
             );
             vertices.push_back({pos, {1.0f, 0.0f, 0.0f}});
@@ -48,5 +49,33 @@ namespace gfx {
 
         mesh.vertices   = vertices;
         mesh.indices    = indices;
+    }
+
+    inline std::vector<vertex> getSphereVertices(const float radius, const int sides) {
+        std::vector<vertex> vertices;
+
+        float latitudeIncrement = glm::radians(180.0f / static_cast<float>(sides));
+        float longitudeIncrement = glm::radians(360.0f / static_cast<float>(sides));
+
+        for (int i = 0; i <= sides; ++i) {
+            float currentLatitudeAngle = -glm::half_pi<float>() + latitudeIncrement * static_cast<float>(i);
+            float currentY = std::sin(currentLatitudeAngle) * radius;
+
+            for (int j = 0; j < sides; ++j) {
+                float currentX = std::cos(longitudeIncrement * static_cast<float>(j)) * std::cos(currentLatitudeAngle) * radius;
+                float currentZ = std::sin(longitudeIncrement * static_cast<float>(j)) * std::cos(currentLatitudeAngle) * radius;
+
+                vertices.push_back({
+                    {currentX, currentY, currentZ},
+                    {1.0f, 0.0f, 0.0f}
+                });
+            }
+        }
+
+        return vertices;
+    }
+
+    inline void makeSphere(Mesh& mesh, const float radius, const int sides) {
+        mesh.vertices = getSphereVertices(radius, sides);
     }
 }
