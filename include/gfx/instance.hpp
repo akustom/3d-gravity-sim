7db1a9ee77927b008c7c8060f2469c01b73df0c1
akustom <dizzaastha@gmail.com>
2026-06-21 17:19:04 +0700

    nothing much

--- include/gfx/instance.hpp
+++ include/gfx/instance.hpp
@@ -2,27 +2,11 @@
 
 #include <vector>
 #include <glad/glad.h>
-#include <glm/glm.hpp>
 #include "mesh.hpp"
 
 
 namespace gfx {
-    struct Instance {
-        glm::dvec4 position;
-
-        void drawInstance(const Mesh& mesh) const {
-            glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(3 * mesh.indices.size()), GL_UNSIGNED_INT, nullptr);
-        }
-    };
-
-
-    struct Instances {
-        std::vector<glm::dvec4> instancePos = {
-            {0.0, 0.0, 0.0, 1.0},
-        };
-
-        void drawInstances(const Mesh& mesh) const {
-            glDrawElementsInstanced(GL_TRIANGLES, static_cast<GLsizei>(3 * mesh.indices.size()), GL_UNSIGNED_INT, nullptr, static_cast<GLsizei>(instancePos.size()));
-        }
-    };
+    void drawInstances(const auto& data, const Mesh& mesh) {
+        glDrawElementsInstanced(GL_TRIANGLES, static_cast<GLsizei>(3 * mesh.indices.size()), GL_UNSIGNED_INT, nullptr, static_cast<GLsizei>(data.size()));
+    }
 }
