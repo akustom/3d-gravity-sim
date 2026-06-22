2d28783f4ec4cfc5204653b8bf0c72c1534d8198
akustom <dizzaastha@gmail.com>
2026-06-22 19:58:03 +0700

    optimized vertex generation a lil for the sphere created the index mapping generator for the sphere

--- include/gfx/mesh.hpp
+++ include/gfx/mesh.hpp
@@ -25,7 +25,7 @@ namespace gfx {
         std::vector<glm::uvec3> indices;
 
         vertices.reserve(sides + 1);
-        vertices.push_back({{0.0f, 0.0f, 0.0f}, {1.0f, 0.0f, 0.0f}});
+        vertices.push_back({{0.0f, 0.0f, 0.0f}, {1.0f, 1.0f, 1.0f}});
 
         float angleIncrement = glm::radians(360.0f / static_cast<float>(sides));
 
@@ -36,7 +36,7 @@ namespace gfx {
                 radius * std::sin(currentAngle),
                 0.0f
             );
-            vertices.push_back({pos, {1.0f, 0.0f, 0.0f}});
+            vertices.push_back({pos, {1.0f, 1.0f, 1.0f}});
         }
 
         for (int i = 1; i <= sides; ++i) {
@@ -54,16 +54,18 @@ namespace gfx {
     inline std::vector<vertex> getSphereVertices(const float radius, const int sides) {
         std::vector<vertex> vertices;
 
-        float latitudeIncrement = glm::radians(180.0f / static_cast<float>(sides));
-        float longitudeIncrement = glm::radians(360.0f / static_cast<float>(sides));
+        float latitudeIncrement     = glm::radians(360.0f / static_cast<float>(sides));
+        float longitudeIncrement    = glm::radians(360.0f / static_cast<float>(sides));
 
-        for (int i = 0; i <= sides; ++i) {
-            float currentLatitudeAngle = -glm::half_pi<float>() + latitudeIncrement * static_cast<float>(i);
+        for (int i = 0; i <= sides / 2; ++i) {
+            float currentLatitudeAngle      = -glm::half_pi<float>() + latitudeIncrement * static_cast<float>(i);
+            float currentLongitudeLength    = std::cos(currentLatitudeAngle);
+
             float currentY = std::sin(currentLatitudeAngle) * radius;
 
             for (int j = 0; j < sides; ++j) {
-                float currentX = std::cos(longitudeIncrement * static_cast<float>(j)) * std::cos(currentLatitudeAngle) * radius;
-                float currentZ = std::sin(longitudeIncrement * static_cast<float>(j)) * std::cos(currentLatitudeAngle) * radius;
+                float currentX = std::cos(longitudeIncrement * static_cast<float>(j)) * currentLongitudeLength * radius;
+                float currentZ = std::sin(longitudeIncrement * static_cast<float>(j)) * currentLongitudeLength * radius;
 
                 vertices.push_back({
                     {currentX, currentY, currentZ},
@@ -75,9 +77,34 @@ namespace gfx {
         return vertices;
     }
 
-    // TODO: create indices generator for sphere
+    inline std::vector<glm::uvec3> getSphereIndices(const int sides) {
+        std::vector<glm::uvec3> indices;
+
+        for (int i = 0; i < sides; ++i) {
+            for (int j = 0; j < sides; ++j) {
+                glm::uvec3 tri1Quad = {
+                    i * sides + j,
+                    i * sides + j + 1,
+                    (i + 1) * sides + j
+                };
+                glm::uvec3 tri2Quad = {
+                    i * sides + j + 1,
+                    (i + 1) * sides + j,
+                    (i + 1) * sides + j + 1,
+                };
+
+                indices.push_back(tri1Quad);
+                indices.push_back(tri2Quad);
+            }
+        }
+        return indices;
+    }
 
     inline void makeSphere(Mesh& mesh, const float radius, const int sides) {
+        for (auto& index : getSphereIndices(sides)) {
+            std::cout << '[' << index.x << ", " << index.y << ", " << index.z << "]\n";
+        }
         mesh.vertices = getSphereVertices(radius, sides);
+        mesh.indices = getSphereIndices(sides);
     }
 }
