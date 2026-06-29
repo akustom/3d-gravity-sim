db9012681a3244a105bb948e97540f9606775998
akustom <dizzaastha@gmail.com>
2026-06-29 15:51:33 +0700

    introduced instanced drawing with base vertex

--- include/gfx/instance.cpp
+++ include/gfx/instance.cpp
@@ -5,6 +5,21 @@
 
 namespace gfx {
     void drawInstances(int instance_count, const Mesh& mesh) {
-        glDrawElementsInstanced(GL_TRIANGLES, static_cast<GLsizei>(3 * mesh.indices.size()), GL_UNSIGNED_INT, nullptr, instance_count);
+        glDrawElementsInstanced(
+            GL_TRIANGLES,
+            mesh.indexCount,
+            GL_UNSIGNED_INT,
+            nullptr,
+            instance_count);
+    }
+
+    void drawInstancesBaseVertex(int instance_count, const Mesh& mesh, const int ebo_offset, const int vbo_offset) {
+        glDrawElementsInstancedBaseVertex(
+            GL_TRIANGLES,
+            mesh.indexCount,
+            GL_UNSIGNED_INT,
+            reinterpret_cast<void*>(ebo_offset * sizeof(unsigned int)),
+            instance_count,
+            vbo_offset);
     }
 }
--- include/gfx/instance.hpp
+++ include/gfx/instance.hpp
@@ -4,4 +4,5 @@
 
 namespace gfx {
     void drawInstances(int instance_count, const Mesh& mesh);
+    void drawInstancesBaseVertex(int instance_count, const Mesh& mesh, int ebo_offset, int vbo_offset);
 }
